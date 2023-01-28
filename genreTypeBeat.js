3d961a82aae706efdfa9b6806bc56cc68dec898e
CeciliaDurst <ycecilia.durst@ufl.edu>
2023-01-28 18:33:39 -0500

    still working on stuff but I wanna see what Allen's done

--- genreTypeBeat.js
+++ genreTypeBeat.js
@@ -1,8 +1,15 @@
 //Determine what genre that beat was like!
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js" integrity="sha512-SGWgwwRA8xZgEoKiex3UubkSkV1zSE1BS6O4pXcaxcNtUlQsOmOmhVnDwIvqGRfEmuz83tIGL13cXMZn6upPyg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 chord1 = 0;
 chord2 = 0;
 chord3 = 0;
 chord4 = 0;
+genre = "empty";
+
+function testGenre(ch1, ch2, ch3, ch4){
+
+}
 
 function testChords(lastChord){
     if(chord1 === 0){
@@ -22,6 +29,7 @@ function testChords(lastChord){
         chord2 = chord3;
         chord3 = chord4;
         chord4 = lastChord;
+        testGenre(chord1, chord2, chord3, chord4);
     }
 
 
