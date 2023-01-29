b42e10d9336054ea994f9452db165df63af3d5df
CeciliaDurst <ycecilia.durst@ufl.edu>
2023-01-28 21:38:25 -0500

    genres

--- genreTypeBeat.js
+++ genreTypeBeat.js
@@ -1,15 +1,57 @@
 //Determine what genre that beat was like!
-//<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js" integrity="sha512-SGWgwwRA8xZgEoKiex3UubkSkV1zSE1BS6O4pXcaxcNtUlQsOmOmhVnDwIvqGRfEmuz83tIGL13cXMZn6upPyg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+
+
 chord1 = 0;
 chord2 = 0;
 chord3 = 0;
 chord4 = 0;
 genre = "empty";
 
-function testGenre(ch1, ch2, ch3, ch4){
+//Hardcoded dataset because I am running out of time :,/
+d1 = ["6","4","1","5","Alternative"];
+d2 = ["4","4","1","5","Catchy"];
+d3 = ["1","1","1","1","Didgeridoo"];
+d4 = ["1","6","4","5","Dreadful"];
+d5 = ["1","6","4","5","Dreadful"];
+d6 = ["1","6","2","4","Endless"];
+d7 = ["1","3","4","6","Energetic"];
+d8 = ["1","5","1","4","Folk"];
+d9 = ["1","6","1","4","Folk"];
+d10 = ["6","5","4","3","Flamenco"];
+d11 = ["6","5","6","5","Flamenco"];
+d12 = ["1","4","3","6","Grunge"];
+d13 = ["2","5","1","6","Jazz"];
+d14 = ["1","4","5","4","Love"];
+d15 = ["1","4","1","5","Memories"];
+d16 = ["1","5","6","4","Pop"];
+d17 = ["1","6","3","7","Pop"];
+d18 = ["4","1","4","5","Rebellious"];
+d19 = ["1","4","5","5","Sad"];
+d20 = ["1","5","4","4","Sad"];
+d21 = ["1","4","5","4","Sad"];
+d22 = ["5","4","1","1","Sweet"];
+d23 = ["1","4","1","4","Simple"];
+d24 = ["1","5","5","1","Simple"];
+d25 = ["1","4","1","4","Wildside"];
+d26 = ["1","1","5","7","Moody"];
+d27 = ["2","1","7","6","Moody"];
 
+var data = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25, d26, d27];
+
+function testGenre(ch1, ch2, ch3, ch4){
+    for(let i = 0; i < data.length; i ++){
+        if(ch1 === parseInt(data[i][0]) && ch2 === parseInt(data[i][1]) && ch3 === parseInt(data[i][2]) && ch4 === parseInt(data[i][3])){
+            genre = data[i][4];
+            ch1 = 0;
+            ch2 = 0;
+            ch3 = 0;
+            ch4 = 0;
+            }
+        }
 }
+
 
 function testChords(lastChord){
     if(chord1 === 0){
@@ -23,15 +65,16 @@ function testChords(lastChord){
     }
     else if(chord4 === 0){
         chord4 = lastChord;
+        testGenre(chord1, chord2, chord3, chord4);
     }
     else {
         chord1 = chord2;
         chord2 = chord3;
         chord3 = chord4;
         chord4 = lastChord;
+        
         testGenre(chord1, chord2, chord3, chord4);
     }
-
 
 }
 
